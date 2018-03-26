933b2bd6cb9aa92884042240375859fe4d0060d3
yuan2009 <yuanxiao200909@gmail.com>
2018-03-26 23:17:01 +0800

    lession9 proc

---
+++ lession9/proc2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import time
+from multiprocessing import Process, Value, Lock
+def func(val, lock):
+    for i in range(50):
+        time.sleep(0.01)
+        with lock:
+            val.value += 1
+
+if __name__ == '__main__':
+    v = Value('i', 0)
+    lock = Lock()
+    procs = [Process(target=func, args=(v, lock)) for i in range(10)]
+    for p in procs:
+        p.start()
+    for p in procs:
+        p.join()
+    print(v.value)
