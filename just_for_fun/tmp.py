1ee4b303616803c30841366ed75e1ba088100fa2
yuan2009 <yuanxiao200909@gmail.com>
2018-05-10 10:58:55 +0800

    test

--- just_for_fun/tmp.py
+++ just_for_fun/tmp.py
@@ -1,18 +1,14 @@
-#!/usr/bin/env python3
-#! -*- utf-8 -*-
-
-class Dog(object):
-    def __init__(self, name):
-        self._name = name
-    def get_name(self):
-        return self._name
-    def set_name(self, value):
-        self._name = value
-    def bark(self):
-        print(self.get_name() + "is sb")
-
-
-
-class Cat(object):
-    def __init__(self):
-        pass
+#!/usr/bin/env python
+import subprocess
+proc_list = ['ceilometer-agent-central',
+                 'ceilometer-agent-notification',
+                 'ceilometer-alarm-evaluator',
+                 'ceilometer-alarm-notifier',
+                 'glance-api']
+for proc in proc_list:
+    cmd = '''ps -ef | grep %s | grep -v grep''' % proc
+    result = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    for i in result.stdout:
+        if i.split()[2] == '1':
+            pid = i.split()[1]
+            print proc, pid
