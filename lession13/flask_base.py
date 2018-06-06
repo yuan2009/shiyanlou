1678ca943dbdc07fcadd518f926cbeaa556d55c2
yuan2009 <yuanxiao200909@gmail.com>
2018-06-06 21:13:31 +0800

    learn

--- lession13/flask_base.py
+++ lession13/flask_base.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
 
+def index():
+    return "hello"
+
+
+
+
+if __name__ == '__main__':
+    app.run()
