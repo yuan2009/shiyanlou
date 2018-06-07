643bca41fa9f2204ec3fd52f86a9ecb6d1d4e837
yuan2009 <yuanxiao200909@gmail.com>
2018-06-07 17:59:11 +0800

    learn

--- lession13/flask_base.py
+++ lession13/flask_base.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route('/')
 
 def index():
-    return "hello"
-
-
+    teacher = {
+        'name': 'yuan',
+        'email': 'yuan@163.com'
+    }
+    course = {
+        'name': 'xiao',
+        'teacher': teacher,
+        'user_count': 5348,
+        'price': 199.0,
+        'lab': None,
+        'is_private': False,
+        'is_member_course': True,
+        'tags': ['python', 'big data', 'Linux']
+    }
+    return render_template('index.html', course=course)
 
 
 if __name__ == '__main__':
